39c930ba6fffe8cb0d2f4884a7dc054a5dd6561c
LuoXianchao <nhjclxc@163.com>
2023-10-30 23:23:46 +0800

    支持项目java -jar启动

--- src/main/java/org/nhjclxc/Main.java
+++ src/main/java/org/nhjclxc/Main.java
@@ -1,7 +1,27 @@
 package org.nhjclxc;
 
+import org.nhjclxc.cg.SpoonCGProcessor;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//        System.out.println("Hello world!");
+        args = new String[]{"E:\\nbu\\other\\java-callgraph-spoon\\src\\main\\java\\org\\nhjclxc\\cg"};
+        if (!(args.length > 0)){
+            System.out.println("jar形式启动项目命令格式：java -jar java-callgraph-spoon-1.0-SNAPSHOT.jar file");
+            System.out.println("file是单个*.java文件或是多个*.java文件列表，也可以是路径");
+            System.out.println("example：");
+            System.out.println("java -jar java-callgraph-spoon-1.0-SNAPSHOT.jar test.java");
+            System.out.println("java -jar java-callgraph-spoon-1.0-SNAPSHOT.jar test.java test2.java test3.java");
+            System.out.println("java -jar java-callgraph-spoon-1.0-SNAPSHOT.jar src\\main\\java\\org\\nhjclxc\\cg");
+        }
+
+        SpoonCGProcessor spoonCGProcessor = new SpoonCGProcessor();
+        System.out.println("Parameter：");
+        for (String arg : args) {
+            System.out.println(arg);
+            spoonCGProcessor.build(arg);
+        }
+
+
     }
 }
--- src/main/java/org/nhjclxc/cg/SpoonCGProcessor.java
+++ src/main/java/org/nhjclxc/cg/SpoonCGProcessor.java
@@ -22,26 +22,24 @@ public class SpoonCGProcessor extends AbstractProcessor<CtMethod<?>> { // 泛型
         element.accept(spoonCGVisitor);
     }
 
+    public void build(String path) {
 
-    public static void main(String[] args) {
         Launcher launcher = new Launcher();
 
         // 设置源码目录
-        String path = "E:\\nbu\\other\\java-callgraph-spoon\\src\\main\\java\\org\\nhjclxc\\cg";
         launcher.addInputResource(path);
         // 设置输出目录（可选）  该输出就是你对代码修改过后的结果，如果你只是对代码进行读取操作那么不需要设备该目录
         String outputPath = "output";
 //        launcher.setSourceOutputDirectory(outputPath);
 
 //         添加自定义Processor (MySpoonProcessor)
-        SpoonCGProcessor spoonCGProcessor = new SpoonCGProcessor();
-        launcher.addProcessor(spoonCGProcessor);
+        launcher.addProcessor(this);
 
         // 运行Spoon
         launcher.run();
 
-        // 获取元素数据
-        Map<String, SpoonCGNode> spoonCGNodeMap = spoonCGProcessor.spoonCGVisitor.getSpoonCGNodeMap();
+        // 获取方法节点数据
+        Map<String, SpoonCGNode> spoonCGNodeMap = this.spoonCGVisitor.getSpoonCGNodeMap();
 
         FileUtils.writeDotFile(outputPath, spoonCGNodeMap);
 
@@ -50,6 +48,15 @@ public class SpoonCGProcessor extends AbstractProcessor<CtMethod<?>> { // 泛型
             System.out.println(k + " ：");
             System.out.println(v);
         });
+
+    }
+
+    public static void main(String[] args) {
+
+        // path可以是文件夹或是单个文件
+        String path = "E:\\nbu\\other\\java-callgraph-spoon\\src\\main\\java\\org\\nhjclxc\\cg";
+        SpoonCGProcessor spoonCGProcessor = new SpoonCGProcessor();
+        spoonCGProcessor.build(path);
 
     }
 }
